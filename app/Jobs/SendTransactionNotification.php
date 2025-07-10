bcd69058d5fb2138eecc932a7d2fd89b76bb5fb4
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2025-07-10 13:26:51 +0000

    fix: change SendTransactionNotification job

--- app/Jobs/SendTransactionNotification.php
+++ app/Jobs/SendTransactionNotification.php
@@ -5,35 +5,60 @@ namespace App\Jobs;
 use App\Models\User;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
-use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Foundation\Bus\Dispatchable;
 
 class SendTransactionNotification implements ShouldQueue
 {
-    use Queueable , Dispatchable;
+    use Queueable, Dispatchable;
 
     public function __construct(
         private User $sender,
         private User $recipient,
-        private int $amount
+        private $amount
     ) {}
 
     public function handle(): void
     {
-        $message = "Você transferiu R$ {$this->amount} para {$this->recipient->name}.";
-        
-        $sender     = mb_convert_encoding($this->sender->name, 'UTF-8', 'UTF-8');
-        $recipient  = mb_convert_encoding($this->recipient->name, 'UTF-8', 'UTF-8');
-        $message    = mb_convert_encoding("Voce transferiu R$ {$this->amount} para {$recipient}.", 'UTF-8', 'UTF-8');
+        $this->sendNotification(
+            to: $this->sender->email,
+            senderName: $this->sender->name,
+            recipientName: $this->recipient->name,
+            message: "Voce transferiu R$ {$this->formatAmount()} para {$this->recipient->name}."
+        );
 
-        $return = Http::post('https://66ad1f3cb18f3614e3b478f5.mockapi.io/v1/send', [
+        $this->sendNotification(
+            to: $this->recipient->email,
+            senderName: $this->sender->name,
+            recipientName: $this->recipient->name,
+            message: "Voce recebeu R$ {$this->formatAmount()} de {$this->sender->name}."
+        );
+
+        Log::info('Transaction notifications sent successfully.');
+    }
+
+    private function sendNotification(string $to, string $senderName, string $recipientName, string $message): void
+    {
+        $sender     = mb_convert_encoding($senderName, 'UTF-8', 'UTF-8');
+        $recipient  = mb_convert_encoding($recipientName, 'UTF-8', 'UTF-8');
+        $message    = mb_convert_encoding($message, 'UTF-8', 'UTF-8');
+
+        $response = Http::post('https://66ad1f3cb18f3614e3b478f5.mockapi.io/v1/send', [
             'sender'     => $sender,
+            'to'         => $to,
             'recipient'  => $recipient,
-            'amount'     => $this->amount,
             'message'    => $message,
         ]);
 
-        Log::info('Notificação enviada com sucesso.');
+        if ($response->failed()) {
+            Log::error('Error sending notification: ' . $response->body());
+        }
+
+    }
+
+    private function formatAmount(): string
+    {
+        return number_format($this->amount, 2, ',', '.');
     }
 }
